64488fc920dde5aed3d6e2607d518cddcc85be83
vlmoon99 <vlmoon@proton.me>
2025-02-17 05:07:36 +0000

    (feature) : add cheks on tinygo and near rs cli

--- main.go
+++ main.go
@@ -7,29 +7,64 @@ import (
 	"os/exec"
 )
 
-var projectName, moduleName string
+type CommandHandler func(args []string)
+
+var commands = map[string]CommandHandler{
+	"create": handleCreate,
+	"build":  handleBuild,
+}
+
+var (
+	projectName string
+	moduleName  string
+)
 
 func main() {
+	if !checkTinyGo() {
+		fmt.Println("TinyGo is not installed. Please visit this link for installation instructions: https://tinygo.org/getting-started/install/")
+	}
+
+	if !checkNearRsCli() {
+		fmt.Println("NEAR CLI RS is not installed. Please visit this link for installation instructions: https://github.com/near/near-cli-rs")
+	}
+
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: cli create -p <projectName> -m <moduleName>")
+		printUsage()
 		return
 	}
 
-	if os.Args[1] == "create" {
-		parseFlags()
-		createProject()
-	} else {
-		fmt.Println("Unknown command:", os.Args[1])
+	command := os.Args[1]
+	handler, exists := commands[command]
+	if !exists {
+		fmt.Println("Unknown command:", command)
+		printUsage()
+		return
 	}
+
+	handler(os.Args[2:])
 }
 
-func parseFlags() {
-	for i := 2; i < len(os.Args)-1; i++ {
-		switch os.Args[i] {
+// Print available commands
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  cli create -p <projectName> -m <moduleName>")
+	fmt.Println("  cli build")
+}
+
+// ---------------- CREATE COMMAND ---------------- //
+
+func handleCreate(args []string) {
+	parseCreateFlags(args)
+	createProject()
+}
+
+func parseCreateFlags(args []string) {
+	for i := 0; i < len(args)-1; i++ {
+		switch args[i] {
 		case "-p":
-			projectName = os.Args[i+1]
+			projectName = args[i+1]
 		case "-m":
-			moduleName = os.Args[i+1]
+			moduleName = args[i+1]
 		}
 	}
 
@@ -40,7 +75,7 @@ func parseFlags() {
 
 func createProject() {
 	fmt.Println("Creating project directory...")
-	if err := runCommand("mkdir", projectName); err != nil {
+	if err := os.Mkdir(projectName, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
 	if err := os.Chdir(projectName); err != nil {
@@ -48,14 +83,10 @@ func createProject() {
 	}
 
 	fmt.Println("Initializing Go module...")
-	if err := runCommand("go", "mod", "init", moduleName); err != nil {
-		log.Fatal(err)
-	}
+	runCommand("go", "mod", "init", moduleName)
 
 	fmt.Println("Installing dependencies...")
-	if err := runCommand("go", "get", "github.com/vlmoon99/near-sdk-go@v0.0.5"); err != nil {
-		log.Fatal(err)
-	}
+	runCommand("go", "get", "github.com/vlmoon99/near-sdk-go@v0.0.5")
 
 	fmt.Println("Creating main.go file...")
 	code := `package main
@@ -68,27 +99,80 @@ import (
 func InitContract() {
 	env.LogString("Init Smart Contract")
 }`
-	if err := writeToFile("main.go", code); err != nil {
-		log.Fatal(err)
-	}
+	writeToFile("main.go", code)
 
 	fmt.Println("Smart contract project created successfully!")
 }
 
-func runCommand(name string, args ...string) error {
+// ---------------- BUILD COMMAND ---------------- //
+
+func handleBuild(args []string) {
+
+	// Check if main.go exists
+	if _, err := os.Stat("main.go"); os.IsNotExist(err) {
+		log.Fatal("Error: Cannot compile. main.go is missing.")
+	}
+
+	fmt.Println("Building smart contract...")
+
+	// Build command for TinyGo
+	buildCmd := []string{
+		"build", "-size", "short", "-no-debug", "-panic=trap",
+		"-scheduler=none", "-gc=leaking", "-o", "main.wasm", "-target", "wasm-unknown", "./",
+	}
+
+	runCommand("tinygo", buildCmd...)
+
+	fmt.Println("Build complete! Generated main.wasm")
+}
+
+// ---------------- UTILITY FUNCTIONS ---------------- //
+
+func runCommand(name string, args ...string) {
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error running command: %s %v\n%v", name, args, err)
+	}
 }
 
-func writeToFile(filename, content string) error {
+func writeToFile(filename, content string) {
 	file, err := os.Create(filename)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 	defer file.Close()
 
 	_, err = file.WriteString(content)
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func isProgramAvailable(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
+}
+
+func checkTinyGo() bool {
+	cmd := exec.Command("tinygo", "version")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error running tinygo:", err)
+		return false
+	}
+	fmt.Println("TinyGo Version:", string(output))
+	return true
+}
+
+func checkNearRsCli() bool {
+	cmd := exec.Command("near", "--version")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error running near:", err)
+		return false
+	}
+	fmt.Println("Near CLI RS Version:", string(output))
+	return true
 }
