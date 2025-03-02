41700c3eaf3d7c35613d50c57013193a5370a56e
vlmoon99 <vlmoon@proton.me>
2025-03-02 11:38:37 +0000

    (feature) : add arm64 ubuntu build

--- build.sh
+++ build.sh
@@ -5,11 +5,13 @@ set -e  # Exit on error
 echo "🚀 Building CLI for all platforms..."
 
 GOOS=linux GOARCH=amd64 go build -o near-cli-linux-amd64
+GOOS=linux GOARCH=arm64 go build -o near-cli-linux-arm64
 GOOS=darwin GOARCH=amd64 go build -o near-cli-mac-amd64
 GOOS=darwin GOARCH=arm64 go build -o near-cli-mac-arm64
 
 echo "📦 Zipping binaries..."
 zip near-cli-linux-amd64.zip near-cli-linux-amd64
+zip near-cli-linux-arm64.zip near-cli-linux-arm64
 zip near-cli-mac-amd64.zip near-cli-mac-amd64
 zip near-cli-mac-arm64.zip near-cli-mac-arm64
 
