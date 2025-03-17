7f1f79599634518d956878bc8dfdee184154867d
Vlad <vlmoon@proton.me>
2025-03-17 18:29:50 +0000

    (feature) : add skeleton

--- go.mod
+++ go.mod
@@ -2,3 +2,9 @@ module github.com/vlmoon99/near-cli-go
 
 go 1.23.5
 
+require github.com/urfave/cli v1.22.16
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.6 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,52 +3,124 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+
+	"github.com/urfave/cli"
 )
 
-type CommandHandler func(args []string)
+func NearCLIWrapper(args ...string) error {
+	cmd := exec.Command("near", args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-var commands = map[string]CommandHandler{
-	"create":             handleCreate,
-	"build":              handleBuild,
-	"deploy":             handleDeploy,
-	"create-dev-account": handleCreateDevAccount,
-	// "import-mainnet-account": handleImportMainnetAccount,
-	"test-package": handleTestPackage,
-	"test-project": handleTestProject,
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error running Near CLI: %w", err)
+	}
+	return nil
 }
+
+// func main() {
+// 	if len(os.Args) < 2 {
+// 		fmt.Println("Please provide a Near CLI command to execute.")
+// 		fmt.Println("Example: ./go-cli-near account")
+// 		return
+// 	}
+
+// 	args := os.Args[1:]
+// if err := NearCLIWrapper(args...); err != nil {
+// 	fmt.Printf("Error: %s\n", err)
+// }
+// }
 
 func main() {
-	if !checkTinyGo() {
-		fmt.Println("TinyGo is not installed. Please visit this link for installation instructions: https://tinygo.org/getting-started/install/")
+	app := &cli.App{
+		Name:  "near-go",
+		Usage: "CLI tool for managing projects on Near Blockchain",
+		Commands: []cli.Command{
+			{
+				Name:  "create",
+				Usage: "Create a new project",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Project created successfully!")
+					return nil
+				},
+			},
+			{
+				Name:  "build",
+				Usage: "Build the project",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Project build completed!")
+					return nil
+				},
+			},
+			{
+				Name:  "deploy",
+				Usage: "Deploy the project to production",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Project deployed to production!")
+					return nil
+				},
+			},
+			{
+				Name:  "account",
+				Usage: "Manage blockchain accounts",
+				Subcommands: []cli.Command{
+					{
+						Name:  "create",
+						Usage: "Create a new account for development",
+						Action: func(c *cli.Context) error {
+							if err := NearCLIWrapper("account"); err != nil {
+								fmt.Printf("Error: %s\n", err)
+							}
+							fmt.Println("Dev account created!")
+							return nil
+						},
+					},
+					{
+						Name:  "import",
+						Usage: "Import an account",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:  "type",
+								Value: "dev",
+								Usage: "Specify account type (e.g., dev, prod)",
+							},
+						},
+						Action: func(c *cli.Context) error {
+							fmt.Printf("Account imported for: %s\n", c.String("type"))
+							return nil
+						},
+					},
+				},
+			},
+			{
+				Name:  "test",
+				Usage: "Run tests",
+				Subcommands: []cli.Command{
+					{
+						Name:  "project",
+						Usage: "Test the project",
+						Action: func(c *cli.Context) error {
+							fmt.Println("Project test completed!")
+							return nil
+						},
+					},
+					{
+						Name:  "package",
+						Usage: "Test the package",
+						Action: func(c *cli.Context) error {
+							fmt.Println("Package test completed!")
+							return nil
+						},
+					},
+				},
+			},
+		},
 	}
 
-	if !checkNearRsCli() {
-		fmt.Println("NEAR CLI RS is not installed. Please visit this link for installation instructions: https://github.com/near/near-cli-rs")
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
 	}
-
-	if len(os.Args) < 2 {
-		printUsage()
-		return
-	}
-
-	command := os.Args[1]
-	handler, exists := commands[command]
-	if !exists {
-		fmt.Println("Unknown command:", command)
-		printUsage()
-		return
-	}
-
-	handler(os.Args[2:])
-}
-
-func printUsage() {
-	fmt.Println("Usage:")
-	fmt.Println("  cli create -p <projectName> -m <moduleName>")
-	fmt.Println("  cli build")
-	fmt.Println("  cli deploy [--prod]")
-	fmt.Println("  cli create-dev-account")
-	fmt.Println("  cli test-package")
-	fmt.Println("  cli test-project")
-	// fmt.Println("  cli import-mainnet-account")
 }
