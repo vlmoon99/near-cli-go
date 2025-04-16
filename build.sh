9fea4eca3c82e65dcefb71b470af7e9dc32de2d9
Vlad <vlmoon@proton.me>
2025-04-16 17:17:29 +0000

    (refactore) : refactre build and add .gitignore

--- build.sh
+++ build.sh
@@ -6,13 +6,11 @@ echo "🚀 Building CLI for all platforms..."
 
 GOOS=linux GOARCH=amd64 go build -o near-cli-linux-amd64
 GOOS=linux GOARCH=arm64 go build -o near-cli-linux-arm64
-GOOS=darwin GOARCH=amd64 go build -o near-cli-mac-amd64
 GOOS=darwin GOARCH=arm64 go build -o near-cli-mac-arm64
 
 echo "📦 Zipping binaries..."
 zip near-cli-linux-amd64.zip near-cli-linux-amd64
 zip near-cli-linux-arm64.zip near-cli-linux-arm64
-zip near-cli-mac-amd64.zip near-cli-mac-amd64
 zip near-cli-mac-arm64.zip near-cli-mac-arm64
 
 echo "✅ Done! Binaries are ready for distribution."
